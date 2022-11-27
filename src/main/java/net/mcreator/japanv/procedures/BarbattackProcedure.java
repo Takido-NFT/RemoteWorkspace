0587abf1bc784aba2199ccf0363f991b5ade70a1
Yoshindo-NFT <byakk@192.168.1.18>
2022-11-27 02:37:14 +0100

    test..encore

---
+++ src/main/java/net/mcreator/japanv/procedures/BarbattackProcedure.java
@@ -0,0 +1,34 @@
+package net.mcreator.japanv.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+
+import net.minecraft.world.entity.Entity;
+
+import net.mcreator.japanv.entity.BarbgeckoEntity;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class BarbattackProcedure {
+	@SubscribeEvent
+	public static void onEntityAttacked(LivingAttackEvent event) {
+		if (event != null && event.getEntity() != null) {
+			execute(event, event.getEntity());
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof BarbgeckoEntity) {
+			((BarbgeckoEntity) entity).animationprocedure = "attack1";
+		}
+	}
+}
--- src/main/java/net/mcreator/japanv/procedures/BarbhurtedProcedure.java
+++ src/main/java/net/mcreator/japanv/procedures/BarbhurtedProcedure.java
@@ -5,6 +5,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
+import net.minecraft.world.entity.Entity;
+
+import net.mcreator.japanv.entity.BarbgeckoEntity;
+
 import javax.annotation.Nullable;
 
 @Mod.EventBusSubscriber
@@ -12,14 +16,19 @@ public class BarbhurtedProcedure {
 	@SubscribeEvent
 	public static void onEntityAttacked(LivingAttackEvent event) {
 		if (event != null && event.getEntity() != null) {
-			execute(event);
+			execute(event, event.getEntity());
 		}
 	}
 
-	public static void execute() {
-		execute(null);
+	public static void execute(Entity entity) {
+		execute(null, entity);
 	}
 
-	private static void execute(@Nullable Event event) {
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof BarbgeckoEntity) {
+			((BarbgeckoEntity) entity).animationprocedure = "hurted";
+		}
 	}
 }
